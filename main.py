94cb504937c428049237a01a73302c03fed861c0
Makana Castillo-Martin <llamakana@gmail.com>
2018-11-07 20:13:03 -0500

    tree is now just an array of buckets. Stash at bucket 0 is a set. Each bucket is an array. Created ORAM class and Ring(Oram) as subclass

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 import os
 import argparse
 import random as rand
-from oram import Node
+# from oram import Node
 from oram import Oram
+from oram import Ring
 import math
+import sys
 
-def level(i):
-    return math.floor(math.log(i+1,2))
 
-def access_n(oram,n):
-    for i in range(n):
-        if (i%1000==0):
-            print("accessing ", i)
-        oram.access(i%oram.n)
-    oram.print_occupancies()
+def access_n(oram,rounds):
+    for i in range(rounds):
+        print("Accessing 1 through n round ", i)
+        for j in range(1,oram.n+1):
+            oram.access(j)
+        oram.print_occupancies()
+    # for i in range(1, n+1):
+    #     if (i%1000==0):
+    #         print("accessing ", i)
+    #     oram.access(i%oram.n)
+    # oram.print_occupancies()
 
 # levels are 0,..l
 # total number of buckets in tree is 2**(l+1)-1
@@ -21,11 +26,13 @@ def access_n(oram,n):
 # eviction rate is a (allowed to approach 2z)
 
 def main(l,a,s,z):
-    oram = Oram(l=l, a=a, s=s, z=z)
+    oram = Ring(l=l, a=a, s=s, z=z)
     oram.init_pos_map()
     oram.init_tree()
+    print("initialized!")
     for i in range(2**oram.l):
         oram.evict()
-    access_n(oram,oram.n*100)
+    print("Beginning accesses")
+    access_n(oram,100)
 
     return oram
--- oram.py
+++ oram.py
@@ -1,89 +1,96 @@
 
 import random as rand
 
-class Node:
-    def __init__(self):
-        self.real_blocks = set()
-        self.counter = 0
-
 class Oram:
-    def __init__(self, l, a, s, z):
-        self.pos_map = {}
-        self.tree = []
-        self.n = a*(2**(l-1))
-        self.stash = set(range(self.n))
+    def __init__(self, l, z):
         self.l = l
-        self.a = a
-        self.s = s
+        self.n = 2**l
         self.z = z
-        self.round = 0
-        self.g = 1
+        self.pos_map = []
+        self.buckets = []
 
     def init_pos_map(self):
-        for i in range(self.n):
-            self.pos_map[i] = rand.randrange((2**self.l) - 1, 2**(self.l+1)-1)
+        self.pos_map = [0] * (self.n+1)
+        for i in range(1, self.n+1):
+            self.pos_map[i] = rand.randint(2**self.l, 2**(self.l+1)-1)
 
     def init_tree(self):
-        self.tree = [None] * (2**(self.l+1)-1)
-        for i in range((2**(self.l+1)-1)):
-            self.tree[i] = Node()
+        self.buckets = [[]] * (2**(self.l+1))
+        self.buckets[0] = set(range(1,self.n+1))
+
+    def print_occupancies(self):
+        for i in range(len(self.buckets)):
+            print("Bucket", i, self.buckets[i])
 
     def get_path(self, leaf):
         i = leaf
         path = [leaf]
-        while i > 0:
+        while i > 1:
             if i%2 == 0:
-                i = (i-2)//2
+                i = i//2
             else:
                 i = (i-1)//2
             path = path + [i]
         return path
 
+
+class Ring(Oram):
+    def __init__(self, l, a, s, z):
+        super().__init__(l, z)
+        self.counters = [0] * (2**(l+1))
+        self.a = a
+        self.s = s
+        self.n = a*(2**(l-1))
+        self.round = 0
+        self.g = 0
+
+
     def evict(self):
-        # leaf = 2**self.l + (self.g - 1)
+        # leaf = 2**self.l + self.g
         # path = self.get_path(leaf)
         # print("evicting from ", leaf)
         # self.g += 1
         # self.g = self.g % (2**self.l)
 
         #randomized evict for now
-        leaf = rand.randrange((2**self.l) - 1, 2**(self.l+1)-1)
+        leaf = rand.randint(2**self.l, 2**(self.l+1)-1)
         path = self.get_path(leaf)
+
         # Read path into stash
         for x in path:
-
-            self.stash = self.stash.union(self.tree[x].real_blocks)
-            self.tree[x].real_blocks = set()
-            # print(len(self.stash))
-        path_len = self.l+1
+            self.buckets[0] = self.buckets[0].union(self.buckets[x])
+            self.buckets[x] = []
+            # print(len(self.buckets[0]))
         i = 0
-        while i < path_len:
+        while i <= self.l:
             current_bucket = path[i]
-            self.tree[current_bucket].counter = 0
+            self.counters[current_bucket] = 0
 
             add_to_bucket = []
             stash_paths = {}
-            for b in self.stash:
+            for b in self.buckets[0]:
                 stash_paths[b] = self.get_path(self.pos_map[b])
 
             for a,p in stash_paths.items():
                 if p[i] == current_bucket:
                     add_to_bucket.append(a)
 
-            available_space = self.z - len(self.tree[current_bucket].real_blocks)
+            available_space = self.z - len(self.buckets[current_bucket])
             # print(self.tree[current_bucket].real_blocks, available_space)
             j = len(add_to_bucket) - 1
             while available_space > 0:
                 if j >= 0:
                     # print("adding ", add_to_bucket[j], " to bucket ", path[i])
-                    self.tree[current_bucket].real_blocks.add(add_to_bucket[j])
-                    # self.stash.remove(add_to_bucket[j])
+                    self.buckets[current_bucket].append(add_to_bucket[j])
+                    # self.buckets[0].remove(add_to_bucket[j])
 
                     # del stash_paths[add_to_bucket[j]]
                     j -= 1
                 available_space -= 1
-            self.stash.difference_update(self.tree[current_bucket].real_blocks)
+            self.buckets[0].difference_update(self.buckets[current_bucket])
             # print("new len", len(stash_paths))
+            del stash_paths
+            del add_to_bucket
             i += 1
 
         return
@@ -91,18 +98,17 @@ class Oram:
     def early_reshuffle(self, leaf):
         path = self.get_path(leaf)
         i = 0
-        path_len = self.l + 1
-        while i < path_len:
+        while i <= self.l:
             current_bucket = path[i]
             stash_paths = {}
             add_to_bucket = []
-            if self.tree[current_bucket].counter > self.s:
-                self.stash = self.stash.union(self.tree[current_bucket].real_blocks)
+            if self.counters[current_bucket] > self.s:
 
-                self.tree[current_bucket].real_blocks = set()
-                self.tree[current_bucket].counter = 0
+                self.buckets[0].update(self.buckets[current_bucket])
+                self.buckets[current_bucket] = []
+                self.counters[current_bucket] = 0
 
-                for b in self.stash:
+                for b in self.buckets[0]:
                     stash_paths[b] = self.get_path(self.pos_map[b])
 
                 for a,p in stash_paths.items():
@@ -111,10 +117,12 @@ class Oram:
                 j = 0
                 while j < self.z:
                     if add_to_bucket:
-                        self.tree[current_bucket].real_blocks.add(add_to_bucket[0])
+                        self.buckets[current_bucket].append(add_to_bucket[0])
                         add_to_bucket = add_to_bucket[1:]
                     j += 1
-                self.stash.difference_update(self.tree[current_bucket].real_blocks)
+                self.buckets[0].difference_update(self.buckets[current_bucket])
+            del stash_paths
+            del add_to_bucket
             i += 1
         return
 
@@ -123,15 +131,15 @@ class Oram:
         leaf = self.pos_map[idx]
         current_path = self.get_path(leaf)
         # re-associate idx with new leaf
-        self.pos_map[idx] = rand.randrange(2**self.l-1, 2**(self.l+1)-1)
+        self.pos_map[idx] = rand.randrange(2**self.l, 2**(self.l+1))
 
         # remove idx from wherever it's stored in the tree if it's there
         for x in current_path:
-            self.tree[x].counter += 1
-            if idx in self.tree[x].real_blocks:
-                self.tree[x].real_blocks.remove(idx)
-        # add idx to stash
-        self.stash.add(idx)
+            self.counters[x] += 1
+            if idx in self.buckets[x]:
+                self.buckets[x].remove(idx)
+                self.buckets[0].add(idx)
+
         # check if we need to evict
         self.round += 1
         if self.round == self.a:
@@ -139,10 +147,4 @@ class Oram:
             self.evict()
 
         self.early_reshuffle(leaf)
-
         return
-
-    def print_occupancies(self):
-        for i in range(len(self.tree)):
-            print(i, self.tree[i].real_blocks)
-        print("Stash", self.stash)
